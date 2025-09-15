ceab06763f632a8967c0dc488fde4482b13dea19
floxcristian <cristianflores.ee@gmail.com>
2025-09-15 15:54:06 -0300

    fix: eliminar usuario demo innecesario en el servicio de usuarios

--- libs/users-domain/src/lib/services/users.service.ts
+++ libs/users-domain/src/lib/services/users.service.ts
@@ -27,17 +27,7 @@ export class UsersService {
       createdAt: new Date(),
     });
 
-    const demo2 = new UserEntity({
-      id: '2',
-      name: 'Super Adminxx',
-      email: 'superadmin@demo.com',
-      phone: '+56912345678',
-      password: hashedPassword,
-      createdAt: new Date(),
-    });
-
     this.users.push(demo);
-    this.users.push(demo2);
     this.currentId = 2;
   }
 
